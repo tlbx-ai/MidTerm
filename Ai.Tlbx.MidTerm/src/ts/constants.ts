df8fa14e0a4474fbb28b02006b160a751de6f6e0
Johannes Schmidt <johannes.schmidt42@gmail.com>
2026-01-09 01:14:20 +0100

    Local release 5.8.3.5 (WebOnly)

--- Ai.Tlbx.MidTerm/src/ts/constants.ts
+++ Ai.Tlbx.MidTerm/src/ts/constants.ts
@@ -138,7 +138,7 @@ export const ICONS = {
   settings: '\ue991',      // wrench
   new: '\uea81',           // terminal
   resize: '\ue989',        // enlarge
-  rename: '\ue922',        // pen-tool
+  rename: '\ue91f',        // drive_file_rename_outline
   close: '\ue909',         // bomb
   menu: '\ue919',          // menu (hamburger)
   update: '\ue91b',        // arrow_right
